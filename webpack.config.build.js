7397f8ca483a471cdd92b55a41b8a750d34574d4
Jason Scott <jpscott07@gmail.com>
2024-08-01 12:57:39 -0400

    get npm deps to at least resolve and install

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,15 +1,14 @@
-const path = require('path');
-const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpackConfig = require('./webpack.config');
+const path = require("path");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpackConfig = require("./webpack.config");
 
 module.exports = Object.assign(webpackConfig, {
-
-    devtool: 'cheap-module-source-map',
+    devtool: "cheap-module-source-map",
 
     output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name].[chunkhash].js'
+        path: path.join(__dirname, "dist"),
+        filename: "[name].[chunkhash].js",
     },
 
     plugins: webpackConfig.plugins.concat([
@@ -17,7 +16,8 @@ module.exports = Object.assign(webpackConfig, {
         //     names: ['manifest']
         // }),
 
-        new CleanWebpackPlugin(['dist'])
-    ])
-
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: ["dist"],
+        }),
+    ]),
 });
