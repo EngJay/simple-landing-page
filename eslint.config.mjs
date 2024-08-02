8db0f1e1ad4180d45e1ab5aced07e07c46f66a42
Jason Scott <jpscott07@gmail.com>
2024-08-02 13:59:24 -0400

    add minimal eslint config for using prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,35 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import js from "@eslint/js";
+import prettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+    {
+        ignores: ["node_modules/", "dist/", "build/"],
+    },
+    {
+        files: ["**/*.js", "**/*.jsx"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+            globals: {
+                browser: true,
+                node: true,
+            },
+        },
+        plugins: {
+            prettier,
+        },
+        rules: {
+            "prettier/prettier": "error",
+            indent: ["error", 2],
+            "linebreak-style": ["error", "unix"],
+            quotes: ["error", "single"],
+            semi: ["error", "always"],
+        },
+    },
 ];
